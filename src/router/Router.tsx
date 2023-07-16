3a73748fe301b864bcfcf2371b5654b2ff6a4a67
Giampietro Piras <giampietro.piras@abinsula.com>
2023-07-16 17:40:38 +0200

    first commit

---
+++ src/router/Router.tsx
@@ -0,0 +1,21 @@
+import { Routes, Route } from 'react-router-dom';
+
+import HomePage from '../pages/HomePage';
+import DetailPage from '../pages/DetailPage';
+
+const Router = () => {
+  return (
+    <Routes>
+      {/* HOME */}
+      <Route path='/' element={<HomePage />} />
+
+      {/* DETAIL */}
+      <Route path='/:userId' element={<DetailPage />} />
+
+      {/* NotFound */}
+      {/* <Route path='*' element={<NotFoundPage />} /> */}
+    </Routes>
+  );
+};
+
+export default Router;
